874e63112e67b5775870021c28781adf950a7157
tamanobi <tamanobi@gmail.com>
2021-03-27 02:15:59 +0900

    feat: 復習

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,21 @@ fn main() {
     book.insert("hogehoge".to_string(), "My favorite".to_string());
     book.insert("fugafuga".to_string(), "My favorite".to_string());
     dbg!(&book);
+    indexing_tuple()
 }
+
+fn indexing_tuple() {
+    let numbers = (1, 2, 3);
+    // Replace below ??? with the tuple indexing syntax.
+    let second = numbers.1;
+
+    assert_eq!(
+    2, second,
+    "This is not the 2nd number in the tuple: {}",
+    second
+    )
+}
+
 
 #[cfg(test)]
 mod tests {
