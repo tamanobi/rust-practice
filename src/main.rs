a3b1536511f65b02a14be4dd0978d1cb7023b88e
tamanobi <tamanobi@gmail.com>
2021-03-31 01:44:32 +0900

    refactor: ファイル分割

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
 use std::collections::HashMap;
 
+mod indexing_tuple;
+
 fn main() {
     let mut book = HashMap::new();
 
     book.insert("hogehoge".to_string(), "My favorite".to_string());
     book.insert("fugafuga".to_string(), "My favorite".to_string());
     dbg!(&book);
-    indexing_tuple();
-    fruit_basket();
+    indexing_tuple::perform();
+    // fruit_basket();
 }
 
-fn indexing_tuple() {
-    let numbers = (1, 2, 3);
-    // Replace below ??? with the tuple indexing syntax.
-    let second = numbers.1;
-
-    assert_eq!(
-    2, second,
-    "This is not the 2nd number in the tuple: {}",
-    second
-    )
-}
 fn fruit_basket() -> HashMap<String, u32> {
     let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
