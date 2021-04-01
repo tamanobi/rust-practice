71e2d57323497439e16836e3bcc080d9472a9fa5
tamanobi <tamanobi@gmail.com>
2021-04-02 01:04:05 +0900

    iter を実装する
    https://docs.microsoft.com/ja-jp/learn/modules/rust-generic-types-traits/7-exercise-implement-iterator

--- src/main.rs
+++ src/main.rs
@@ -64,4 +64,61 @@ mod tests {
         assert_eq!(Container::new(-12).value, -12);
         assert_eq!(Container::new(Some("text")).value, Some("text"));
     }
+
+    #[test]
+    fn implement_iter() {
+        struct Groups<T> {
+            inner: Vec<T>
+        }
+
+        impl<T> Groups<T> {
+            fn new(inner: Vec<T>) -> Self {
+                Groups { inner }
+            }
+        }
+
+        impl<T: PartialEq> Iterator for Groups<T> {
+            type Item = Vec<T>;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.inner.is_empty() {
+                    return None
+                }
+
+                let mut cursor = 1;
+                let first = &self.inner[0];
+                for element in &self.inner[1..] {
+                    if element == first {
+                        cursor += 1;
+                    } else {
+                        break;
+                    }
+                }
+                let items = self.inner.drain(0..cursor).collect();
+                Some(items)
+            }
+        }
+
+        let data = vec![1, 2, 3, 4];
+        assert_eq!(
+            Groups::new(data).into_iter().collect::<Vec<Vec<_>>>(),
+            vec![
+                vec![1],
+                vec![2],
+                vec![3],
+                vec![4],
+            ],
+        );
+
+        let data2 = vec![1, 1, 3, 4, 3, 3];
+        assert_eq!(
+            Groups::new(data2).into_iter().collect::<Vec<Vec<_>>>(),
+            vec![
+                vec![1, 1],
+                vec![3],
+                vec![4],
+                vec![3, 3],
+            ],
+        )
+    }
 }
